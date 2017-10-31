3157cefc80c3b08183acb7c915a9f2d82b4a8191
jroper1130 <warbeast78@hotmail.com>
2017-10-31 02:45:14 -0700

    Add files via upload

---
+++ ChallengeSimpleCalculator/ChallengeSimpleCalculator/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeSimpleCalculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Simple Calculator</h1>
+        <p>
+            &nbsp;</p>
+        <div>
+            First Value:<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            Second Value:<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="+" />
+&nbsp;<asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="-" />
+&nbsp;<asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="*" />
+&nbsp;<asp:Button ID="Button4" runat="server" OnClick="Button4_Click" style="width: 19px" Text="/" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server" style="font-family: Arial, Helvetica, sans-serif; background-color: #3366FF"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeSimpleCalculator/ChallengeSimpleCalculator/Default.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeSimpleCalculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int first = int.Parse(TextBox1.Text);
+            int second = int.Parse(TextBox2.Text);
+            int result = first + second;
+            resultLabel.Text = result.ToString(); 
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            int first = int.Parse(TextBox1.Text);
+            int second = int.Parse(TextBox2.Text);
+            int result = first - second;
+            resultLabel.Text = result.ToString();
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            int first = int.Parse(TextBox1.Text);
+            int second = int.Parse(TextBox2.Text);
+            int result = first * second;
+            resultLabel.Text = result.ToString();
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            double first = double.Parse(TextBox1.Text);
+            double second = double.Parse(TextBox2.Text);
+            double result = first / second;
+            resultLabel.Text = result.ToString();
+
+        }
+    }
+}
